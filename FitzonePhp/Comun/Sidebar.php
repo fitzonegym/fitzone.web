c2354771f2df77cacb06919984ce70b6229c63ea
abril ruiz <abrilruiz01@live.com.ar>
2024-06-09 05:37:19 -0300

    Redistribucion

---
+++ FitzonePhp/Comun/Sidebar.php
@@ -0,0 +1,40 @@
+    <!-- ======= Sidebar ======= -->
+    <aside id="sidebar" class="sidebar">
+      <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+          <a class="nav-link " data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-person"></i><span>Socios</span><i class="bi bi-chevron-down ms-auto"></i>
+          </a>
+          <ul id="forms-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+            <li>
+              <a href="AltaSocio.php" class="active">
+              <i class="bi bi-file-earmark-plus"></i><span>Registrar nuevo socio</span>
+              </a>
+            </li>
+            <li>
+              <a href="Socios.php" class="active">
+              <i class="bi bi-file-earmark-plus"></i><span>Listado de socios</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-cash-coin"></i><span>Membresias</span><i class="bi bi-chevron-down ms-auto"></i>
+          </a>
+          <ul id="forms-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+            <li>
+              <a href="viaje_carga.php" class="active">
+              <i class="bi bi-file-earmark-plus"></i><span>Cargar Nueva Membresia</span>
+              </a>
+            </li>
+            <li>
+              <a href="viajes_listado.php" class="active">
+              <i class="bi bi-layout-text-window-reverse"></i><span>Listado de Membresias</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </aside><!-- End Sidebar-->
